277b4341feae9e777a8912ef32375d0fca39c308
R-T <csri16001@gmail.com>
2021-05-07 19:42:22 +0900

    sec5-58

---
+++ store/reducers/user.js
@@ -0,0 +1,23 @@
+
+const initialState = {
+	clips: []
+}
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'ADD_CLIP':
+			return {
+				...state,
+				clips=[...state.clips,action.clip],
+			}
+		case 'DELETE_CLIP':
+			return {
+				...state,
+				clip: state.clips.filter(clip => clip.url !== action.clip.url),
+			}
+		default:
+			return state;
+	}
+}
+
+export default reducer
