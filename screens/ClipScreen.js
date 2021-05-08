4e9cfe3f94d8416e14f217df7c65c01cecfe8a78
R-T <csri16001@gmail.com>
2021-05-09 04:58:04 +0900

    sec8-62

---
+++ screens/ClipScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+})
+
+export default ClipScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>CLIP SCREEN</Text>
+    </SafeAreaView>
+  )
+}
