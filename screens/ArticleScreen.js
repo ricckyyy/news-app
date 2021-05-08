91c8b2ca2a40000b3710dea6518d3b05c85cf189
R-T <csri16001@gmail.com>
2021-05-09 04:24:12 +0900

    sec 8 - 61

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,19 +1,36 @@
-import React from "react"
-import { StyleSheet, Text, SafeAreaView } from "react-native"
-import { WebView } from "react-native-webview"
+import React from 'react'
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { WebView } from 'react-native-webview'
+import { useDispatch } from 'react-redux'
+import { addClip, deleteClip } from '../store/actions/user'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
 })
 
 export default ArticleScreen = ({ route }) => {
   const { article } = route.params
-  // console.log(articles)
+  const dispatch = useDispatch()
+
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(addClip({ clip: article }))
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(deleteClip({ clip: article }))
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
+      </TouchableOpacity>
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   )
--- store/index.js
+++ store/index.js
@@ -1,9 +1,10 @@
 import { combineReducers, createStore } from 'redux'
 import userReducer from './reducers/user'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 const rootReducer = combineReducers({
   user: userReducer,
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools())
 export default store
